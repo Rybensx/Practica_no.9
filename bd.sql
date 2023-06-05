b4a18048ff528dafb11580109b2e05d6a68b9ec0
Rybensx <anderson_arf@hotmail.es>
2023-06-04 23:05:42 -0600

    Error campo null, tabla ventas.venta_producto

--- bd.sql
+++ bd.sql
@@ -5,3 +5,24 @@ create table productos(
     producto_situacion smallint not null default 1,
     primary key (producto_id)
 )
+
+
+
+CREATE TABLE clientes (
+    cliente_id serial PRIMARY KEY NOT NULL,
+    cliente_nombre varchar(100) NOT NULL,
+    cliente_nit varchar(10) NOT NULL,
+    cliente_situacion smallint NOT NULL DEFAULT 1
+)
+
+
+
+CREATE TABLE ventas (
+    venta_id serial PRIMARY KEY NOT NULL,
+    venta_producto integer NOT NULL REFERENCES productos(producto_id),
+    venta_cliente integer NOT NULL REFERENCES clientes(cliente_id),
+    venta_fecha datetime year to minute NOT NULL,
+    cantidad integer NOT NULL,
+    total decimal(10,2) NOT NULL
+)
+
